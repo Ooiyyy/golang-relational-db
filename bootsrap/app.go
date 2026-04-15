c92fcd35dfb20ec52884f8c7c6b6bc6e800f2fcb
Ooiyyy <oiypes59@gmail.com>
2026-04-15 08:29:56 +0700

    nothing

--- bootsrap/app.go
+++ bootsrap/app.go
@@ -19,23 +19,19 @@ func Run() {
 	// 3) Rangkai dependensi dari lapisan paling bawah ke atas:
 	// repository -> service -> handler.
 	userRepo := mysql.NewUserRepository(db)
-	todoRepo := mysql.NewTodoRepository(db)
 	subjectRepo := mysql.NewSubjectRepo(db)
 	scoreRepo := mysql.NewScoreRepo(db)
 
 	userService := service.NewUserService(userRepo)
-	todoService := service.NewTodoService(todoRepo)
 	subjectService := service.NewSubjectService(subjectRepo)
 	scoreService := service.NewScoreService(scoreRepo)
 
 	authHandler := handler.NewAuthHandler(userService, jwtSecret)
-	todoHandler := handler.NewTodoHandlers(todoService)
 	subjectHandler := handler.NewSubjectHandler(subjectService)
 	scoreHandler := handler.NewScoreHandler(scoreService)
 
 	r := routes.SetupRouter(
 		authHandler,
-		todoHandler,
 		subjectHandler,
 		scoreHandler,
 		jwtSecret,
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -6,14 +6,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRouter(authHandler *handler.AuthHandler, todoHandler *handler.TodoHandler, subjectHandler *handler.SubjectHandler, scoreHandler *handler.ScoreHandler, jwtSecret string) *gin.Engine {
+func SetupRouter(authHandler *handler.AuthHandler, subjectHandler *handler.SubjectHandler, scoreHandler *handler.ScoreHandler, jwtSecret string) *gin.Engine {
 	// gin.Default() menambahkan logger + recovery middleware bawaan.
 	r := gin.Default()
 	r.Static("/uploads", "./uploads")
 
 	// Daftarkan endpoint per modul.
 	AuthRoute(r, authHandler, jwtSecret)
-	TodoRoute(r, todoHandler)
 	SubjectRoute(r, subjectHandler, jwtSecret)
 	ScoreRoute(r, scoreHandler, jwtSecret)
 	// Router ini dipanggil oleh bootstrap untuk menerima request dari client.
